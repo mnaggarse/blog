875c580336376782668506af4cdb28a96cc26498
mnaggarse <mnaggarse@gmail.com>
2025-07-08 02:59:59 +0300

    Add user model, router, controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,26 +1,23 @@
 import * as UserModel from "../models/user.model.js";
 
-export const getUserById = async (req, res) => {
-  const { id } = req.params;
-
+export const getAllUsers = async (_req, res) => {
   try {
-    const user = await UserModel.getUserById(id);
-    res.status(200).json(user);
+    const users = await UserModel.getAllUsers();
+    res.status(200).json(users);
   } catch (err) {
-    console.error("Error in createUser controller:", err);
+    console.error("Error in getAllUsers controller:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const createUser = async (req, res) => {
-  const { name, email, password, image, description } = req.body;
+  const { name, email, password } = req.body;
 
   try {
     const user = await UserModel.getUserByEmail(email);
-    
     if (user) return res.status(400).json({ message: "User already exists" });
 
-    await UserModel.createUser({ name, email, password, image, description });
+    await UserModel.createUser({ name, email, password });
     res.status(200).json({ message: "User created successfully" });
   } catch (err) {
     console.error("Error in createUser controller:", err);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,8 +1,8 @@
 import database from "../config/database.js";
 
-export const getUserById = async (id) => {
-  const result = await database.query("select * from users where id = $1", [id]);
-  return result.rows[0];
+export const getAllUsers = async () => {
+  const result = await database.query("select * from users");
+  return result.rows;
 };
 
 export const getUserByEmail = async (email) => {
@@ -10,9 +10,10 @@ export const getUserByEmail = async (email) => {
   return result.rows[0];
 };
 
-export const createUser = async ({ name, email, password, image, description }) => {
-  await database.query(
-    "insert into users (name, email, password, image, description) values ($1, $2, $3, $4, $5)",
-    [name, email, password, image, description]
-  );
+export const createUser = async ({ name, email, password }) => {
+  await database.query("insert into users (name, email, password) values ($1, $2, $3)", [
+    name,
+    email,
+    password,
+  ]);
 };

e76079f3bf5c2e80d901b06b6b8a41b97ee1d353
mnaggarse <mnaggarse@gmail.com>
2025-07-08 03:00:16 +0300

    Add comments router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import postRoutes from "./routers/post.route.js";
-import userRoutes from "./routers/user.route.js";
+import commentRoutes from "./routers/comment.router.js";
+import postRoutes from "./routers/post.router.js";
+import userRoutes from "./routers/user.router.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 // routers
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");

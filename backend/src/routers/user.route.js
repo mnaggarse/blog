809a9b893721d017a4bb5440ad9aa1b15b3b5db0
mnaggarse <mnaggarse@gmail.com>
2025-07-08 03:01:11 +0300

    Deleted uesr router file

--- backend/src/routers/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, getUserById } from "../controllers/user.controller.js";
-
-const router = express.Router();
-
-router.get("/:id", getUserById);
-router.post("/", createUser);
-
-export default router;

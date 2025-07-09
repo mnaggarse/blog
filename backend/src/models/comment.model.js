5b698394e77f2e7bd988235d3e02b1a947954548
mnaggarse <mnaggarse@gmail.com>
2025-07-09 15:17:01 +0300

    Add get comment by id and remove delete comment

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -5,14 +5,15 @@ export const getAllComments = async () => {
   return result.rows;
 };
 
+export const getCommentById = async () => {
+  const result = await database.query("select * from comments where id = $1", [id]);
+  return result.rows[0];
+};
+
 export const createComment = async ({ content, user_id, post_id }) => {
   await database.query("insert into comments (content, user_id, post_id) values ($1, $2, $3)", [
     content,
     user_id,
     post_id,
   ]);
-};
-
-export const deleteComment = async (id) => {
-  await database.query("delete from comments where id = $1", [id]);
 };

1759fbf6374cba9f72ca7ca5320938415a078c46
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:27:53 +0300

    Better naming for functions and parameters

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -1,19 +1,14 @@
 import database from "../config/database.js";
 
-export const getAllComments = async () => {
-  const result = await database.query("select * from comments");
+export const getCommentsByPostId = async (postId) => {
+  const result = await database.query("select * from comments where post_id = $1", [postId]);
   return result.rows;
 };
 
-export const getCommentById = async () => {
-  const result = await database.query("select * from comments where id = $1", [id]);
-  return result.rows[0];
-};
-
-export const createComment = async ({ content, user_id, post_id }) => {
+export const createComment = async ({ content, userId, postId }) => {
   await database.query("insert into comments (content, user_id, post_id) values ($1, $2, $3)", [
     content,
-    user_id,
-    post_id,
+    userId,
+    postId,
   ]);
 };

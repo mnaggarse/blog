f3315c8e908cc9244a2be940eeec8210f398f7c9
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:26:14 +0300

    Update like functions

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,10 +1,15 @@
 import * as LikeModel from "../models/like.model.js";
+import * as PostModel from "../models/post.model.js";
+import * as UserModel from "../models/user.model.js";
 
-export const getLikesCount = async (req, res) => {
-  const { post_id } = req.body;
+export const getPostLikes = async (req, res) => {
+  const { postId } = req.params;
 
   try {
-    const likesCount = await LikeModel.getLikesCount(post_id);
+    const post = await PostModel.getPostById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const likesCount = await LikeModel.getPostLikes(postId);
     res.status(200).json(likesCount);
   } catch (err) {
     console.error("Error in getLikesCount controller:", err);
@@ -13,16 +18,23 @@ export const getLikesCount = async (req, res) => {
 };
 
 export const toggleLike = async (req, res) => {
-  const { user_id, post_id } = req.body;
+  const { postId } = req.params;
+  const { userId } = req.body;
 
   try {
-    const isLiked = await LikeModel.isLiked({ user_id, post_id });
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const post = await PostModel.getPostById(postId);
+    if (!post) return res.status(400).json({ message: "Post not found" });
+
+    const isLiked = await LikeModel.isLiked({ userId, postId });
 
     if (isLiked) {
-      await LikeModel.removeLike({ user_id, post_id });
+      await LikeModel.removeLike({ userId, postId });
       res.status(200).json({ message: "Like removed successfully" });
     } else {
-      await LikeModel.addLike({ user_id, post_id });
+      await LikeModel.addLike({ userId, postId });
       res.status(200).json({ message: "Like added successfully" });
     }
   } catch (err) {

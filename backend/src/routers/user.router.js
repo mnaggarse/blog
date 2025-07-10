fbc6aee19a5b49db6625b1126955e10ae697c47d
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:24:58 +0300

    Add post endpoints to user

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createUser, getAllUsers } from "../controllers/user.controller.js";
+import { createPost, getUserPosts, deleteUserPosts, deletePostById } from "../controllers/post.controller.js";
+import { createUser, getAllUsers, getUserById } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/", createUser);
+router.get("/:userId", getUserById);
+router.get("/:userId/posts", getUserPosts);
+router.post("/:userId/posts", createPost);
+router.delete("/:userId/posts", deleteUserPosts);
+router.delete("/:userId/posts/:postId", deletePostById);
 
 export default router;

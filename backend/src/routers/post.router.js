d83c6e0731271de5b4a3748d45075dde7ac2fd9a
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:26:00 +0300

    Add comment and like routes to post

--- backend/src/routers/post.router.js
+++ backend/src/routers/post.router.js
@@ -1,7 +1,7 @@
 import express from "express";
+import { createComment, getPostComments } from "../controllers/comment.controller.js";
+import { getPostLikes, toggleLike } from "../controllers/like.controller.js";
 import {
-  createPost,
-  deleteAllPosts,
   deletePostById,
   getAllPosts,
   getPostById,
@@ -11,10 +11,12 @@ import {
 const router = express.Router();
 
 router.get("/", getAllPosts);
-router.get("/:id", getPostById);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/", deleteAllPosts);
-router.delete("/:id", deletePostById);
+router.get("/:postId", getPostById);
+router.patch("/:postId", updatePost);
+router.delete("/:postId", deletePostById);
+router.get("/:postId/comments", getPostComments);
+router.post("/:postId/comments", createComment);
+router.get("/:postId/likes", getPostLikes);
+router.post("/:postId/likes", toggleLike);
 
 export default router;

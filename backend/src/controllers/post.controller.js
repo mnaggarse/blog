f0e6d3d785786d608a27d06f293691d11fd4570b
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:26:42 +0300

    Add getUserPosts and update other functions

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import * as PostModel from "../models/post.model.js";
+import * as UserModel from "../models/user.model.js";
 
 export const getAllPosts = async (_req, res) => {
   try {
@@ -10,11 +11,26 @@ export const getAllPosts = async (_req, res) => {
   }
 };
 
-export const getPostById = async (req, res) => {
-  const { id } = req.params;
+export const getUserPosts = async (req, res) => {
+  const { userId } = req.params;
 
   try {
-    const post = await PostModel.getPostById(id);
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const posts = await PostModel.getUserPosts(userId);
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error("Error in getUserPosts controller:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getPostById = async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const post = await PostModel.getPostById(postId);
     if (!post) return res.status(404).json({ message: "Post not found" });
 
     res.status(200).json(post);
@@ -25,10 +41,14 @@ export const getPostById = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const { title, description, image, content, user_id } = req.body;
+  const { userId } = req.params;
+  const { title, description, image, content } = req.body;
 
   try {
-    await PostModel.createPost({ title, description, image, content, user_id });
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    await PostModel.createPost({ title, description, image, content, userId });
     res.status(200).json({ message: "Post created successfully" });
   } catch (err) {
     console.error("Error in createPost controller:", err);
@@ -37,45 +57,53 @@ export const createPost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
-  const { id } = req.params;
+  const { postId } = req.params;
   const { title, description, image, content } = req.body;
 
   try {
-    const post = await PostModel.getPostById(id);
+    const post = await PostModel.getPostById(postId);
     if (!post) return res.status(404).json({ message: "Post not found" });
 
-    await PostModel.updatePost({ title, description, image, content, id });
+    await PostModel.updatePost({ title, description, image, content, postId });
     res.status(200).json({ message: "Post updated successfully" });
   } catch (err) {
-    console.error("Error in deleteAllPosts controller:", err);
+    console.error("Error in updatePost controller:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export const deleteAllPosts = async (_req, res) => {
+export const deleteUserPosts = async (req, res) => {
+  const { userId } = req.params;
+
   try {
-    const posts = await PostModel.getAllPosts();
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const posts = await PostModel.getUserPosts(userId);
     if (posts.length == 0) return res.status(400).json({ message: "There is no posts" });
 
-    await PostModel.deleteAllPosts();
+    await PostModel.deleteUserPosts(userId);
     res.status(200).json({ message: "All posts deleted successfully" });
   } catch (err) {
-    console.error("Error in deleteAllPosts controller:", err);
+    console.error("Error in deleteUserPosts controller:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export const deletePostById = async (c, res) => {
-  const { id } = req.params;
+export const deletePostById = async (req, res) => {
+  const { userId, postId } = req.params;
 
   try {
-    const post = await PostModel.getPostById(id);
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const post = await PostModel.getPostById(postId);
     if (!post) return res.status(404).json({ message: "Post not found" });
 
-    await PostModel.deletePostById(id);
+    await PostModel.deletePostById({ userId, postId });
     res.status(200).json({ message: "Post deleted successfully" });
   } catch (err) {
-    console.error("Error in deleteAllPosts controller:", err);
+    console.error("Error in deletePostById controller:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };

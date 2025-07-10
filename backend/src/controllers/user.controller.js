796c82f67b158d99294953623569360c5a499f87
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:27:15 +0300

    Add getUserById and update other functions

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -10,14 +10,28 @@ export const getAllUsers = async (_req, res) => {
   }
 };
 
+export const getUserById = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error("Error in getAllUsers controller:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, image, description } = req.body;
 
   try {
     const user = await UserModel.getUserByEmail(email);
     if (user) return res.status(400).json({ message: "User already exists" });
 
-    await UserModel.createUser({ name, email, password });
+    await UserModel.createUser({ name, email, password, image, description });
     res.status(200).json({ message: "User created successfully" });
   } catch (err) {
     console.error("Error in createUser controller:", err);

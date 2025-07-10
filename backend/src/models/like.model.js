f6e0f1f59e0c26117203b15bcb53ca864ee15bf8
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:24:12 +0300

    Update names of functions and parameters

--- backend/src/models/like.model.js
+++ backend/src/models/like.model.js
@@ -1,22 +1,22 @@
 import database from "../config/database.js";
 
-export const getLikesCount = async (post_id) => {
-  const result = await database.query("select count(*) from likes where post_id = $1", [post_id]);
+export const getPostLikes = async (postId) => {
+  const result = await database.query("select count(*) from likes where post_id = $1", [postId]);
   return result.rows[0];
 };
 
-export const addLike = async ({ user_id, post_id }) => {
-  await database.query("insert into likes (user_id, post_id) values ($1, $2)", [user_id, post_id]);
+export const addLike = async ({ userId, postId }) => {
+  await database.query("insert into likes (user_id, post_id) values ($1, $2)", [userId, postId]);
 };
 
-export const removeLike = async ({ user_id, post_id }) => {
-  await database.query("delete from likes where user_id = $1 and post_id = $2", [user_id, post_id]);
+export const removeLike = async ({ userId, postId }) => {
+  await database.query("delete from likes where user_id = $1 and post_id = $2", [userId, postId]);
 };
 
-export const isLiked = async ({ user_id, post_id }) => {
+export const isLiked = async ({ userId, postId }) => {
   const result = await database.query("select * from likes where user_id = $1 and post_id = $2", [
-    user_id,
-    post_id,
+    userId,
+    postId,
   ]);
   return result.rows[0];
 };

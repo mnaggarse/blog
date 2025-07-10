435b7e1982314b675fe7e4cb47e62bf53ff1cb2d
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:23:11 +0300

    Update getPostComments and createPost

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,20 +1,34 @@
 import * as CommentModel from "../models/comment.model.js";
+import * as PostModel from "../models/post.model.js";
+import * as UserModel from "../models/user.model.js";
 
-export const getAllComments = async (_req, res) => {
+export const getPostComments = async (req, res) => {
+  const { postId } = req.params;
+
   try {
-    const comments = await CommentModel.getAllComments();
+    const post = await PostModel.getPostById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const comments = await CommentModel.getCommentsByPostId(postId);
     res.status(200).json(comments);
   } catch (err) {
-    console.error("Error in getAllComments controller:", err);
+    console.error("Error in getPostComments controller:", err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const createComment = async (req, res) => {
-  const { content, user_id, post_id } = req.body;
+  const { postId } = req.params;
+  const { content, userId } = req.body;
 
   try {
-    await CommentModel.createComment({ content, user_id, post_id });
+    const user = await UserModel.getUserById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const post = await PostModel.getPostById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    await CommentModel.createComment({ content, userId, postId });
     res.status(200).json({ message: "Comment created successfully" });
   } catch (err) {
     console.error("Error in createComment controller:", err);

9d025568d0adf3287ab160e14adc91c53f56234f
mnaggarse <mnaggarse@gmail.com>
2025-07-10 16:25:18 +0300

    Delete files

--- backend/src/routers/comment.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createComment, getAllComments } from "../controllers/comment.controller.js";
-
-const router = express.Router();
-
-router.get("/", getAllComments);
-router.post("/", createComment);
-
-export default router;
--- backend/src/routers/like.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getLikesCount, toggleLike } from "../controllers/like.controller.js";
-
-const router = express.Router();
-
-router.get("/", getLikesCount);
-router.post("/", toggleLike);
-
-export default router;
